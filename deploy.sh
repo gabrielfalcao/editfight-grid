a8152438d04fdb7642ca39462378a4d2f87d4436
Steven Degutis <sbdegutis@gmail.com>
2018-04-21 18:48:44 -0500

    Style gifs page.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
 
-scp -r index.js public ubuntu@editfight.com:app
+scp index.js public/{gifs,index}.html ubuntu@editfight.com:app
 ssh ubuntu@editfight.com sudo service editfight restart
